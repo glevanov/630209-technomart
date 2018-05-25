efb7656e63a43140f04e24529902e340525db10d
Grigoriy Levanov <glevanov@gmail.com>
2018-05-25 20:42:34 +0300

    Добавляет интерактивную карту

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 'use strict';
+const mapLink = document.querySelector('.js-map-link');
+const mapFrame = document.querySelector('.js-map-iframe');
+
+mapLink.classList.add('visually-hidden');
+mapFrame.classList.remove('visually-hidden');
